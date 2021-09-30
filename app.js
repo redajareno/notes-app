0b90ee85cb20ab9840c8305132f5e628d52b55dd
Reno Redaja <rredaja@radialspark.com>
2021-09-30 12:17:39 -1000

    Update notes-app

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const msg = getNotes();
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(msg);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+})
 
-console.log(chalk.green.inverse.bold('Success!'));
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title);
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing your notes',
+    handler: function () {
+        console.log('Listing out all notes');
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+})
+
+yargs.parse();
